43068d187f507c0e65a7e7355e319fbd1ef8305b
h0wter <howter99@gmail.com>
2022-10-04 21:35:10 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,71 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { GlobalStyle } from './GlobalStyle';
+import { SearchBar } from './SearchBar/SearchBar';
+import { getImages } from 'Services/api';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { Button } from './Button/Button';
+import { Box } from './Box';
+import { AppContainer } from './App.styled';
+import { Loader } from './Loader/Loader';
+
+export class App extends Component {
+  state = {
+    images: [],
+    query: '',
+    page: 1,
+    loading: false,
+  };
+
+  componentDidUpdate = async (_, prevState) => {
+    if (
+      prevState.query !== this.state.query ||
+      prevState.page !== this.state.page
+    ) {
+      this.setState({ loading: true });
+      const images = await getImages(this.state.query, this.state.page);
+      this.setState(prevState => {
+        return {
+          images: [...prevState.images, ...images.hits],
+          loading: false,
+        };
+      });
+    }
+  };
+
+  onSubmit = async e => {
+    const query = e.target.input.value.trim();
+    if (this.state.query === query) {
+      return;
+    }
+    this.setState({ images: [], query, page: 1 });
+  };
+
+  onLoadMore = () => {
+    this.setState(prevState => {
+      return { page: prevState.page + 1 };
+    });
+  };
+
+  render() {
+    const { images, loading } = this.state;
+    return (
+      <AppContainer>
+        <SearchBar onSubmit={this.onSubmit} />
+
+        {images.length > 0 && <ImageGallery data={images} />}
+
+        {images.length > 0 && !loading && (
+          <Button onClick={this.onLoadMore}>Load More</Button>
+        )}
+
+        {loading && (
+          <Box ml="auto" mr="auto" width="80px">
+            <Loader />
+          </Box>
+        )}
+
+        <GlobalStyle />
+      </AppContainer>
+    );
+  }
+}
